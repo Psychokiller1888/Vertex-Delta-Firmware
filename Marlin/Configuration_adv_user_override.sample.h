3063995dea8a31d3e313c6392f1fc6857577ee47
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-02-03 13:37:20 +0100

    sample files

---
+++ Marlin/Configuration_adv_user_override.sample.h
@@ -0,0 +1,16 @@
+/*
+  This file is provided as an easy config editor for users that need to quick change an option
+  without navigating in the huge Configuration_adv.h
+  To modify a setting, you must first unset the constant!
+*/
+
+#ifndef CONFIGURATION_ADV_USER_OVERRIDE_H
+#define CONFIGURATION_ADV_USER_OVERRIDE_H
+
+
+// Exemple to set the delta radius
+//#undef THERMAL_PROTECTION_BED_PERIOD
+//#define THERMAL_PROTECTION_BED_PERIOD 40
+
+
+#endif
